3ce208c333a46d78fd4e40763f88f3477450a384
Manish Kumar <manishkushwaha0152@gmail.com>
2025-07-19 16:31:58 +0530

    Update constants.js

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://api.render.com/deploy/srv-d1t0u93uibrs738oul20?key=BeyfI8QooMU/api";
+export const BASE_URL = "https://api.render.com/deploy/srv-d1t0u93uibrs738oul20?key=BeyfI8QooMU";
  
