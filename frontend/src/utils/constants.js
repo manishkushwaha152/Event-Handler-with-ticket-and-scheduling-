fc1b445e7506327030fc009987aef3003c937043
Manish Kumar <manishkushwaha0152@gmail.com>
2025-07-19 16:30:59 +0530

    Update constants.js

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://event-handler-with-ticket-and-scheduling.onrender.com";
+export const BASE_URL = "https://api.render.com/deploy/srv-d1t0u93uibrs738oul20?key=BeyfI8QooMU/api";
  
